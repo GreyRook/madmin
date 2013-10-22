8f1270a0a0efed2198ef373bae5c06cdb90a5e62
Dominik Schacht <domschacht@gmail.com>
2013-10-22 16:52:45 +0200

    increased version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Web based administration tool for mongodb',
     long_description=""""A tool to view and edit mongodb instances similar to PhpMyAdmin.\n
     MAdmin allows you to manage databases, collections and documents. """,
-    version='0.1.1',
+    version='0.2.0',
     author='Dominik Schacht',
     author_email='domschacht@gmail.com',
     install_requires=['rueckenwind==0.2.0', 'motor==0.1.1', 'PyMongo==2.5.0'],
